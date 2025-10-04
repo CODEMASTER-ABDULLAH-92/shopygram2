4ae485891c95c0bd591a17c1c94daa6ce583236a
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-10-04 22:40:23 +0500

    update the app

--- src/app/storeProvider.tsx
+++ src/app/storeProvider.tsx
@@ -1,17 +1,27 @@
-"use client";
-import { useRef } from "react";
-import { Provider } from "react-redux";
-import { makeStore, AppStore } from "../../src/app/lib/store";
-export default function StoreProvider({
-  children,
-}: {
-  count: number;
-  children: React.ReactNode;
-}) {
-  const storeRef = useRef<AppStore | null>(null);
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
+// "use client";
+// import { useRef } from "react";
+// import { Provider } from "react-redux";
+// import { makeStore, AppStore } from "../../src/app/lib/store";
+// export default function StoreProvider({
+//   children,
+// }: {
+//   count: number;
+//   children: React.ReactNode;
+// }) {
+//   const storeRef = useRef<AppStore | null>(null);
+//   if (!storeRef.current) {
+//     storeRef.current = makeStore();
+//   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+//   return <Provider store={storeRef.current}>{children}</Provider>;
+// }
+
+'use client';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '@/app/lib/store';
+
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
 }
